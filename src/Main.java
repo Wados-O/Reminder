05445af91a8c5a0325f6e58135d4252cb9598793
Natalya Selyunina <seluninanv@mail.ru>
2023-08-10 21:39:06 +0300

    feat : HashMap

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,18 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
+
+  Map<String, Map<String, LocalDateTime>> tasks = new HashMap<>();
 
   public static void main(String[] args) throws IOException {
     Scanner sc = new Scanner(System.in);
@@ -52,7 +56,7 @@ public class Main {
   }
 
   public static void remove(Scanner sc) {
-
+    System.out.print("Введите задачу, которую хотите удалить: ");
   }
 
   public static void addTask(Scanner sc) throws IOException{
