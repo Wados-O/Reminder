d6b0920acdf22028747989096badf578b856f6e9
Natalya Selyunina <seluninanv@mail.ru>
2023-08-10 21:55:08 +0300

    fix : HashMap, add ArrayList

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,19 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
 
-  Map<String, Map<String, LocalDateTime>> tasks = new HashMap<>();
+  List<Task> tasks = new ArrayList<>();
 
   public static void main(String[] args) throws IOException {
     Scanner sc = new Scanner(System.in);
