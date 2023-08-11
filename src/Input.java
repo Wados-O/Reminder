c46a3a9c7bb814f9ace8a14c4fe86a5f53b5d865
Natalya Selyunina <seluninanv@mail.ru>
2023-08-11 23:03:00 +0300

    feat : Input.java

--- src/Input.java
+++ src/Input.java
@@ -1,20 +1,31 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Input {
-public static void readTaskFromFile(List<Task> taskList)throws IOException{
-  Scanner sc;
-  sc = new Scanner(System.in);
-  File file = new File("res/tasks.txt");
-  sc = new Scanner(file);
 
-  while (sc.hasNextLine()) {
-    String line = sc.nextLine();
-    // Process the line as needed
-    // Обрабатывайте строку по мере необходимости
+  private static String ourFile = "res/tasks.txt";
+  protected static List<Task> arrayList = new ArrayList<>();
+
+  public static void readTaskFromFile(List<Task> tasks, Scanner scanner) throws IOException {
+  scanner = new Scanner(new File(ourFile));
+
+  while (scanner.hasNextLine()) {
+    String line = scanner.nextLine();
+    arrayList.add(line);
     System.out.println(line);
   }
-}
+  scanner.close();
+  }
+
+  public static void closeFileWithSaving(List<String> tasks, File file, Scanner scanner) throws IOException {
+    FileWriter outFile = new FileWriter(file);
+    for (Task line : tasks) {
+      outFile.write(String.valueOf(line));
+      outFile.close();
+    }
+  }
 }
