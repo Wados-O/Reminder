e0dc17d2162910fdadac39aa1a05cb6a447c2849
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-11 19:51:04 +0200

    feat : add Constructor and Getter to enum Category

--- src/Priority.java
+++ src/Priority.java
@@ -1,9 +1,23 @@
 public enum Priority {
-  HIGH(),
-  MEDIUM,
-  LOW(),
+  HIGH(1,"HIGH"),
+  MEDIUM(2,"MEDIUM"),
+  LOW(3,"LOW"),
       ;
+private int num;
+private String priority;
 
-  Priority() {
+  Priority(int num, String priority) {
+    this.num = num;
+    this.priority = priority;
   }
+
+  public int getNum() {
+    return num;
+  }
+
+  public String getPriority() {
+    return priority;
+  }
+
+
 }
