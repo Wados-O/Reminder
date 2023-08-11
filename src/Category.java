76c33b777f3ef566c505c236a417a7e05797c708
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-11 23:15:32 +0200

    feat : add getNameCategory

--- src/Category.java
+++ src/Category.java
@@ -1,16 +1,30 @@
 public enum Category {
-HOME(1),
-  WORK(2),
-  SHOPPING(3),
-  PERSONAL(4),
-  HEALTH(5);
+ HOME(1,"HOME"),
+  WORK(2,"WORK"),
+  SHOPPING(3,"SHOPPING"),
+  PERSONAL(4,"PERSONAL"),
+  HEALTH(5,"HEALTH");
 
   private final int num;
-  Category(int num) {
+private String nameCategory;
+  Category(int num, String nameCategory) {
   this.num = num;
+   this.nameCategory = nameCategory;
   }
 
   public int getNum() {
     return num;
   }
+
+ public String getNameCategory() {
+  return nameCategory;
+ }
+
+ @Override
+ public String toString() {
+  return "Category{" +
+         "num=" + num +
+         ", nameCategory='" + nameCategory + '\'' +
+         '}';
+ }
 }
