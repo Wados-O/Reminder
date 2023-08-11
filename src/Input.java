d62d7bf8acdea5932640205bb880b4c3672e26b2
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-11 18:46:54 +0200

    feat : add readTaskFromFile

--- src/Input.java
+++ src/Input.java
@@ -1,3 +1,20 @@
-public class Input {
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
 
+public class Input {
+public static void readTaskFromFile(List<Task> taskList)throws IOException{
+  Scanner sc;
+  sc = new Scanner(System.in);
+  File file = new File("res/tasks.txt");
+  sc = new Scanner(file);
+
+  while (sc.hasNextLine()) {
+    String line = sc.nextLine();
+    // Process the line as needed
+    // Обрабатывайте строку по мере необходимости
+    System.out.println(line);
+  }
+}
 }
--- src/Runner.java
+++ src/Runner.java
@@ -3,5 +3,4 @@ public class Runner {
   public static void main(String[] args) {
 
   }
-
 }
