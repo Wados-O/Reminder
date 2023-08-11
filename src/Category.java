e259d5a6420cb68910adc1bd142635d82fe66d8f
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-11 19:47:52 +0200

    feat : add Constructor and Getter to enum Category

--- src/Category.java
+++ src/Category.java
@@ -1,9 +1,16 @@
 public enum Category {
-HOME,
-  WORK,
-  SHOPPING,
-  PERSONAL,
-  HEALTH,
-  Category() {
+HOME(1),
+  WORK(2),
+  SHOPPING(3),
+  PERSONAL(4),
+  HEALTH(5);
+
+  private final int num;
+  Category(int num) {
+  this.num = num;
+  }
+
+  public int getNum() {
+    return num;
   }
 }
