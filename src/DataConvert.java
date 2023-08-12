66f3ed1dde5217c1ea66763a35940887321fb20e
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-13 01:38:33 +0200

    feat : add checkFormatDate

--- src/DataConvert.java
+++ src/DataConvert.java
@@ -3,6 +3,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DataConvert {
+
   public static Date parseDate(String dateStr) {
     SimpleDateFormat dateFormatDash = new SimpleDateFormat("dd-MM-yyyy");
     SimpleDateFormat dateFormatComa = new SimpleDateFormat("dd,MM,yyyy");
@@ -14,11 +15,11 @@ public class DataConvert {
         return dateFormatDash.parse(dateStr);
       } else if (dateStr.contains(".")) {
         return dateFormatDot.parse(dateStr);
-      }  else if (dateStr.contains(",")) {
+      } else if (dateStr.contains(",")) {
         return dateFormatComa.parse(dateStr);
       } else if (dateStr.contains("/")) {
         return dateFormatSlash.parse(dateStr);
-      }else {
+      } else {
         System.out.println("Неверный формат даты. Используйте [dd.MM.yyyy].");
         return null;
       }
@@ -29,9 +30,23 @@ public class DataConvert {
       return null;
     }
   }
+
   public static String formatDate(Date date) {
     SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
     return dateFormat.format(date);
   }
 
+  public static boolean checkFormatDate(String dateStr) {
+    boolean result = false;
+    if (dateStr != null) {
+      int day = Integer.parseInt(dateStr.substring(0, 2));
+      int month = Integer.parseInt(dateStr.substring(3, 5));
+      if (month >= 1 && month <= 12) {
+        if (day >= 1 && day <= 31) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 }
