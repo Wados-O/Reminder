472f1f8a3831583ed7c8c4d39ec065b34dafea0e
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-12 17:02:27 +0200

    feat : add class secondSuspend ,thirdSuspend , forthSuspend

--- src/Suspend.java
+++ src/Suspend.java
@@ -1,14 +1,39 @@
 public class Suspend {
+
   /**
    * Here are tools to delay presentation in project!!!
    */
 
-  public static void longSuspend (){
+  public static void longSuspend() {
     int suspend = 2000;
     long start = System.currentTimeMillis();
-    while (start >= System.currentTimeMillis() - suspend){
+    while (start >= System.currentTimeMillis() - suspend) {
       longSuspend();
     }
   }
 
+  public static void secondSuspend() {
+    int suspend = 500;
+    long start = System.currentTimeMillis();
+    while (start >= System.currentTimeMillis() - suspend) {
+      secondSuspend();
+    }
+  }
+
+  public static void thirdSuspend() {
+    int suspend = 100;
+    long start = System.currentTimeMillis();
+    while (start >= System.currentTimeMillis() - suspend) {
+      thirdSuspend();
+    }
+  }
+
+  public static void forthSuspend() {
+    int suspend = 500;
+    long start = System.currentTimeMillis();
+    while (start >= System.currentTimeMillis() - suspend) {
+      forthSuspend();
+    }
+
+  }
 }
