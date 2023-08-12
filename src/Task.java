813abf580eb5faef21966d433ddeb4efe81474e2
Natalya Selyunina <seluninanv@mail.ru>
2023-08-12 16:57:05 +0300

    fix : Task.java

--- src/Task.java
+++ src/Task.java
@@ -2,33 +2,33 @@ import java.time.LocalDateTime;
 
 public class Task {
 
-  private int count = 0;
-  private final int id;
-  private final Category category;
+//  private int count = 0;
+//  private final int id;
+//  private final Category category;
   private final String title;
   private final String message;
-  private final Priority priority;
-  private final LocalDateTime expiredDate;
+//  private final Priority priority;
+//  private final LocalDateTime expiredDate;
 
-  public Task(Priority priority, Category category, String title, String message, LocalDateTime expiredDate) {
-    this.category = category;
+  public Task(String title, String message) {
+//    this.category = category;
     this.title = title;
     this.message = message;
-    this.priority = priority;
-    this.expiredDate = expiredDate;
+//    this.priority = priority;
+//    this.expiredDate = expiredDate;
   }
 
-  public int getCount() {
-    return count;
-  }
-
-  public void setCount(int count) {
-    this.count = count;
-  }
-
-  public int getId() {
-    return id;
-  }
+//  public int getCount() {
+//    return count;
+//  }
+//
+//  public void setCount(int count) {
+//    this.count = count;
+//  }
+//
+//  public int getId() {
+//    return id;
+//  }
 
   public String getTitle() {
     return title;
@@ -38,15 +38,20 @@ public class Task {
     return message;
   }
 
-  public LocalDateTime getExpiredDate() {
-    return expiredDate;
-  }
+//  public LocalDateTime getExpiredDate() {
+//    return expiredDate;
+//  }
+//
+//  public Category getCategory() {
+//    return category;
+//  }
+//
+//  public Priority getPriority() {
+//    return priority;
+//  }
 
-  public Category getCategory() {
-    return category;
-  }
-
-  public Priority getPriority() {
-    return priority;
+  @Override
+  public String toString() {
+    return "Наименование: '" + title + '\'' + " Описание: '" + message + "\'";
   }
 }
