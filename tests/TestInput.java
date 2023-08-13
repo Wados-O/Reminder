2271f4555c4aaa2628477cf7a789e5f7f7cc2e3c
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-13 20:22:24 +0200

    feat :add TestInput.java

---
+++ tests/TestInput.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.text.ParseException;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class TestInput {
+
+}
