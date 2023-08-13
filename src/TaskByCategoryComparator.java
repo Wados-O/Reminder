873efc642be953b6335c946c9ea5d0193fde25a1
Natalya Selyunina <seluninanv@mail.ru>
2023-08-14 01:40:18 +0300

    fix : TaskByCategoryComparator.java

--- src/TaskByCategoryComparator.java
+++ src/TaskByCategoryComparator.java
@@ -4,6 +4,10 @@ public class TaskByCategoryComparator implements Comparator<Task> {
 
   @Override
   public int compare(Task task1, Task task2) {
-    return task1.getCategory().getNum() - task2.getCategory().getNum();
+    int category = task1.getCategory().getNum() - task2.getCategory().getNum();
+    if (category != 0) {
+      return category;
+    }
+    return task1.getPlaneDate().compareTo(task2.getPlaneDate());
   }
 }
