e81c5b52e831aaa249ce9636ec4109f1dca74755
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-13 20:22:10 +0200

    feat :add MethodsTest

---
+++ tests/MethodsTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.text.ParseException;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MethodsTest {
+
+}
