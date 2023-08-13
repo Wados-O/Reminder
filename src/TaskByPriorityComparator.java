6a6deb5733bd3f2f0c1b860bdf6676518bfcfa34
Natalya Selyunina <seluninanv@mail.ru>
2023-08-14 01:36:33 +0300

    fix : TaskByPriorityComparator.java

--- src/TaskByPriorityComparator.java
+++ src/TaskByPriorityComparator.java
@@ -4,6 +4,10 @@ public class TaskByPriorityComparator implements Comparator<Task> {
 
   @Override
   public int compare(Task task1, Task task2) {
-    return task1.getPriority().getNum() - task2.getPriority().getNum();
+    int priority = task1.getPriority().getNum() - task2.getPriority().getNum();
+    if (priority != 0) {
+      return priority;
+    }
+    return task1.getPlaneDate().compareTo(task2.getPlaneDate());
   }
 }
