b32141a9c1fb96b3b8a51ee3dff2ef20a53d0509
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-14 14:25:25 +0200

    open to presentation

--- src/Runner.java
+++ src/Runner.java
@@ -5,23 +5,23 @@ public class Runner {
 
   public static void main(String[] args) throws IOException {
 
-//Intro.logoTeam();
-//Intro.logoPresents();
-//Intro.emptyPlace();
-//Intro.stringLoading();
-//Intro.speedJump();
-//Intro.logoProject();
-//Intro.emptyPlace();
-//Intro.stringDownload();
-//Suspend.longSuspend();
-//Intro.speedJump();
-//Intro.speedJump();
+    Intro.logoTeam();
+    Intro.logoPresents();
+    Intro.emptyPlace();
+    Intro.stringLoading();
+    Intro.speedJump();
+    Intro.logoProject();
+    Intro.emptyPlace();
+    Intro.stringDownload();
+    Suspend.longSuspend();
+    Intro.speedJump();
+    Intro.speedJump();
     Input.readTaskFromFile();
 //    System.out.println(Menu.SHOW_MENU_REFACTOR);
 //    System.out.println(Menu.SHOW_CATEGORY_MENU);
 //    System.out.println(Menu.SHOW_PRIORITY_CHOICE);
 //    System.out.println(Menu.SHOW_SORT_MENU);
 
-Methods.firstMenu(new Scanner(System.in));
+    Methods.firstMenu(new Scanner(System.in));
   }
 }
