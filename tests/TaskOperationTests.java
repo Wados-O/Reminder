a92356ad9a5e9aea480c9d25ece21d708d31d0e7
Natalya Selyunina <seluninanv@mail.ru>
2023-08-20 22:36:57 +0300

    feat : test remove()

--- tests/TaskOperationTests.java
+++ tests/TaskOperationTests.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -8,18 +9,30 @@ import java.util.Date;
 class TaskOperationTests {
 
   Scanner scanner = new Scanner(System.in);
-
   List<Task> tasks = new ArrayList<>();
 
   @Test
   public void remove() {
-    Task task = new Task("Title", "Message", Category.PERSONAL, Priority.HIGH,
-        DataConvert.parseDate(scanner.nextLine()), new Date(), false);
+    String title = "title";
+    String message = "message";
+    Category category = Category.PERSONAL;
+    Priority priority = Priority.HIGH;
+    Date planeDate = DataConvert.parseDate("15/09/2023");
+    Date createdDate = DataConvert.parseDate(String.valueOf(LocalDate.now()));
+    Boolean isDone = false;
 
-    Task task2 = new Task("Title2", "Message2", Category.HEALTH, Priority.MEDIUM,
-        DataConvert.parseDate(scanner.nextLine()), new Date(), true);
-
+    Task task  = new Task(title, message,category, priority,planeDate, createdDate, isDone);
     tasks.add(task);
+
+    String title2 = "title2";
+    String message2 = "message2";
+    Category category2 = Category.HOME;
+    Priority priority2 = Priority.MEDIUM;
+    Date planeDate2 = DataConvert.parseDate("20/09/2023");
+    Date createdDate2 = DataConvert.parseDate(String.valueOf(LocalDate.now()));
+    Boolean isDone2 = true;
+
+    Task task2  = new Task(title2, message2,category2, priority2,planeDate2, createdDate2, isDone2);
     tasks.add(task2);
 
     int number = 1;
@@ -27,6 +40,3 @@ class TaskOperationTests {
     tasks.remove(indexTasks);
   }
 }
-
-
-
