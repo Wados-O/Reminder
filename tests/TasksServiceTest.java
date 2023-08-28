40aea9e74c2141f81825feb6d705f98ce29642d1
Natalya Selyunina <seluninanv@mail.ru>
2023-08-28 15:28:49 +0300

    feat : test correctingPriority()

--- tests/TasksServiceTest.java
+++ tests/TasksServiceTest.java
@@ -226,6 +226,32 @@ class TasksServiceTest {
 
   @Test
   void correctingPriority() {
+    Task task1 = new Task("title", "message", Category.PERSONAL, Priority.HIGH,
+        DataConvert.parseDate("15/09/2023"), DataConvert.parseDate(String.valueOf(
+        LocalDate.now())), true);
+
+    Task task2 = new Task("title2", "message2", Category.HEALTH, Priority.MEDIUM,
+        DataConvert.parseDate("21/12/2023"), DataConvert.parseDate(String.valueOf(
+        LocalDate.now())), false);
+
+    Task task3 = new Task("title3", "message3", Category.HOME, Priority.LOW,
+        DataConvert.parseDate("14/10/2023"), DataConvert.parseDate(String.valueOf(
+        LocalDate.now())), true);
+
+    tasksService.add(task1);
+    tasksService.add(task2);
+    tasksService.add(task3);
+
+    int indexTask = 2;
+    Priority priority = Priority.MEDIUM;
+
+    tasksService.correctingPriority(indexTask, priority);
+
+    assertEquals(
+        2, tasksService.getTasks().get(2).getPriority().getNum());
+
+    tasksService.getTasks().clear();
+    assertTrue(tasksService.getTasks().isEmpty());
   }
 
   @Test
