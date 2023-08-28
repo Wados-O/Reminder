17dd2fb5bfea6472bc8f4ee60db69b0ee0d64974
Natalya Selyunina <seluninanv@mail.ru>
2023-08-28 15:08:51 +0300

    fix : Test changeThreeTasksStatus(), changeTwoTasksStatus()

--- tests/TasksServiceTest.java
+++ tests/TasksServiceTest.java
@@ -33,27 +33,54 @@ class TasksServiceTest {
   }
 
   @Test
-  void changeStatus() {
-//    List<Task> tasks = new ArrayList<>();
-//    Task task1 = new Task("title", "message", Category.PERSONAL, Priority.HIGH,
-//        DataConvert.parseDate("15/09/2023"), DataConvert.parseDate(String.valueOf(
-//        LocalDate.now())), true);
-//
-//    Task task2 = new Task("title2", "message2", Category.HEALTH, Priority.MEDIUM,
-//        DataConvert.parseDate("21/12/2023"), DataConvert.parseDate(String.valueOf(
-//        LocalDate.now())), false);
-//
-//    Task task3 = new Task("title3", "message3", Category.HOME, Priority.LOW,
-//        DataConvert.parseDate("14/10/2023"), DataConvert.parseDate(String.valueOf(
-//        LocalDate.now())), true);
-//
-//    tasks.add(task1);
-//    tasks.add(task2);
-//    tasks.add(task3);
-//
-//    int indexTask = 2;
-//    int choiceStatus = 1;
-//    taskList.changeStatus(indexTask, choiceStatus);
+  void changeThreeTasksStatus() {
+    Task task1 = new Task("title", "message", Category.PERSONAL, Priority.HIGH,
+        DataConvert.parseDate("15/09/2023"), DataConvert.parseDate(String.valueOf(
+        LocalDate.now())), true);
+
+    Task task2 = new Task("title2", "message2", Category.HEALTH, Priority.MEDIUM,
+        DataConvert.parseDate("21/12/2023"), DataConvert.parseDate(String.valueOf(
+        LocalDate.now())), false);
+
+    Task task3 = new Task("title3", "message3", Category.HOME, Priority.LOW,
+        DataConvert.parseDate("14/10/2023"), DataConvert.parseDate(String.valueOf(
+        LocalDate.now())), true);
+
+    tasksService.add(task1);
+    tasksService.add(task2);
+    tasksService.add(task3);
+
+    int indexTask = 2;
+    int choiceStatus = 1;
+    tasksService.changeStatus(indexTask, choiceStatus);
+
+    assertTrue(tasksService.getTasks().get(2).isDone());
+
+    tasksService.getTasks().clear();
+    assertTrue(tasksService.getTasks().isEmpty());
+  }
+
+  @Test
+  void changeTwoTasksStatus() {
+    Task task1 = new Task("title", "message", Category.PERSONAL, Priority.HIGH,
+        DataConvert.parseDate("15/09/2023"), DataConvert.parseDate(String.valueOf(
+        LocalDate.now())), true);
+
+    Task task2 = new Task("title2", "message2", Category.HEALTH, Priority.MEDIUM,
+        DataConvert.parseDate("21/12/2023"), DataConvert.parseDate(String.valueOf(
+        LocalDate.now())), false);
+
+    tasksService.add(task1);
+    tasksService.add(task2);
+
+    int indexTask = 1;
+    int choiceStatus = 1;
+    tasksService.changeStatus(indexTask, choiceStatus);
+
+    assertTrue(tasksService.getTasks().get(1).isDone());
+
+    tasksService.getTasks().clear();
+    assertTrue(tasksService.getTasks().isEmpty());
   }
 
   @Test
@@ -70,18 +97,13 @@ class TasksServiceTest {
         DataConvert.parseDate("14/10/2023"), DataConvert.parseDate(String.valueOf(
         LocalDate.now())), true);
 
-    System.out.println(tasksService.getTasksCount());
-    System.out.println(tasksService.getTasks().toString());
     tasksService.add(task1);
     tasksService.add(task2);
     tasksService.add(task3);
 
-    System.out.println(tasksService.getTasksCount());
-    System.out.println(tasksService.getTasks().toString());
     int indexTask = 1;
 
     tasksService.removeTask(indexTask);
-    System.out.println(tasksService.getTasksCount());
 
     assertEquals(2, tasksService.getTasksCount());
     assertEquals(task3, tasksService.getTasks().get(1));
